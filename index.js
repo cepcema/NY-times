f363a105fa8c22b35413905b935cab47cb0824a5
cepcema <qvor_dimitrov@hotmail.com>
2022-08-10 16:57:26 +0200

    initil commit 2

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const feedDisplay = document.querySelector(`#feed`)
 
-const url = `https://ny-times-api.herokuapp.com/test`
+const url = `https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=7jwnRSBpbCTs4Nq50AO3Anq7hGPG8uvW`
 
 fetch(url)
 .then(response => response.json())
-.then(data => data.forEach(article => {
+.then(data => data.results.forEach(article => {
     const title =  `<h3>---- ${article.title} -----</h3>`
     feedDisplay.insertAdjacentHTML(`beforeend`, title)
-}));
+} ))
+// .then(data => data.forEach(article => {
+//     const title =  `<h3>---- ${article.title} -----</h3>`
+//     feedDisplay.insertAdjacentHTML(`beforeend`, title)
+// }));

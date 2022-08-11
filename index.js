2535d6177d725e9a983ca71327adef1ad56f6675
cepcema <49753804+cepcema@users.noreply.github.com>
2022-08-11 13:38:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const feedDisplay = document.querySelector(`#feed`)
-
+const feedDisplay = document.querySelector(`.row`)
+console.log(feedDisplay)
 const url = `https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=7jwnRSBpbCTs4Nq50AO3Anq7hGPG8uvW`
 
 fetch(url)
 .then(response => response.json())
 .then(data => data.results.forEach(article => {
-    const title =  `<h3>---- ${article.title} -----</h3>`
+    const title =  `<div class="col-md-6">
+    <div class="tn-img">
+        <img src="${article.media[0][`media-metadata`][2].url}" />
+        <div class="tn-content">
+            <div class="tn-content-inner">
+                <a class="tn-date" href=""><i class="far fa-clock"></i>${article.updated}</a>
+                <a class="tn-title" href="">${article.title}</a>
+            </div>
+        </div>
+    </div>
+</div>`
+//console.log(article.media[0][`media-metadata`][2].url)
+console.log(article)
     feedDisplay.insertAdjacentHTML(`beforeend`, title)
 } ))
 // .then(data => data.forEach(article => {
